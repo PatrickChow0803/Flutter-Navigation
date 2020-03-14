7ae1c54354fe80a971035bb5506ac9870cd6459b
patrick <patrickchow0803@gmail.com>
2020-03-14 13:25:07 -0400

    Added basic navigation between 2 screens

--- Navigation-Flutter-Demo-master/lib/main.dart
+++ Navigation-Flutter-Demo-master/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screen0.dart';
 import 'screen1.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Screen1(),
+      home: Screen0(),
+      routes: {},
     );
   }
 }
