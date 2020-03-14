5f3bfca1e9925ce6665c4f52a6734f9f1d99052d
patrick <patrickchow0803@gmail.com>
2020-03-14 13:38:17 -0400

    Added named routes to navigate

--- Navigation-Flutter-Demo-master/lib/main.dart
+++ Navigation-Flutter-Demo-master/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screen0.dart';
 import 'screen1.dart';
+import 'screen2.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,8 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Screen0(),
-      routes: {},
+//      home: Screen0(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Screen0(),
+        '/first': (context) => Screen1(),
+        '/second': (context) => Screen2(),
+      },
     );
   }
 }
